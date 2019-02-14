e9add6849816110d37d0953a5a9b73f8d9216c27
hanleyjames <hanley.doth@outlook.com>
2019-02-14 14:59:13 -0800

    Write First model test failing

---
+++ PBScraperTests/PBScrapeModelsTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using PBScraper.Models;
+
+namespace PBScraperTests
+{
+    [TestClass]
+    public class PBScraperModelTests
+    {
+        [TestMethod]
+        public void PBScrape_SavesInstance_IsTrue()
+        {
+            Assert.AreEqual(true, false);
+        }
+    }
+}
